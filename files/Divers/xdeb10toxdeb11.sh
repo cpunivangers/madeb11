944e77354ccc331cef6f249d2b025db33bdbc8cd
cpunivangers <christian.pottier@univ-angers.fr>
2023-07-21 14:49:45 +0200

    Version 11.230721.1449

---
+++ files/Divers/xdeb10toxdeb11.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+ 
+# 16/02/2022
+# christian.pottier@univ-angers.fr
+#  
+# XDEB10 to XDEB11
+
+#~ # test bash
+#~ if [ -z $BASH ]; then
+	#~ echo "Lancer avec bash !"
+	#~ exit 1
+#~ fi
+
+sudo apt -y purge madeb10
+sudo rm -rf /opt/madeb
+sudo rm -rf /opt/deb_perso
+sudo rm -rf /opt/ansible
+
+sudo apt -y purge virtualbox-6.1
+sudo apt -y purge ".*:i386"
+sudo dpkg --remove-architecture i386
+sudo apt autoremove -y
+
+curl -sL https://uabox.univ-angers.fr/index.php/s/A529vAkMqi5X5n1/download > update_madeb11.sh
+
+bash update_madeb11.sh download
+
+sudo rm /etc/apt/sources.list.d/*
+sudo cp /opt/ansible/madeb11/files/sources.list /etc/apt/sources.list
+sudo apt update -y --allow-releaseinfo-change
+#Espace disque nécéssaire
+sudo apt -o APT::Get::Trivial-Only=true full-upgrade
+read tempo
+
+sudo DEBIAN_FRONTEND=noninteractive apt -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" full-upgrade -y
+
+cd /opt/ansible/madeb11
+sh madeb.sh
+
+# Suppression des fichiers config restant
+sudo apt liste '~c'
+sudo apt purge '~c'
---
+++ files/update_madeb11.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+ 
+# 20/10/2021
+# christian.pottier@univ-angers.fr
+#
+if [ -z $BASH ]; then
+	echo "Lancer avec bash !"
+	exit
+fi
+
+# Maj Debian
+sudo apt update -y --allow-releaseinfo-change
+sudo apt full-upgrade -y
+# maj madeb Debian 11
+# sudo apt -y install curl
+# curl -sL https://raw.githubusercontent.com/cpunivangers/madeb11/main/files/update_madeb11.sh | bash
+OPTION="$1"
+
+## Nouvelle version environnement ansible pip
+# Vérif ansible installé
+if ! ( sudo apt-cache policy ansible | grep Installé | grep -q aucun ) ; then
+	sudo apt-get -y remove ansible
+fi
+
+# Vérif environnement ansible pip installé
+## Création environnement virtuel python
+if ( sudo apt-cache policy python3-venv | grep Installé | grep -q aucun ) ; then
+	sudo apt-get -y install python3-venv
+fi
+sudo mkdir -m 2755 -p /opt/ansible
+sudo chown -R $USER:sudo /opt/ansible
+python3 -m venv /opt/ansible/vansible
+
+## Activation environnement
+source /opt/ansible/vansible/bin/activate
+## Mise à jour pip sous l'environnement python
+python3 -m pip install --upgrade pip
+## Installation ansible
+python3 -m pip install --upgrade ansible
+ansible --version
+## Ajout pour machine windows
+python3 -m pip install --upgrade pywinrm
+sudo mkdir -m 2755 -p /usr/share/ansible/collections
+sudo chown -R $USER:sudo /usr/share/ansible/collections
+ansible-galaxy collection install ansible.windows -p /usr/share/ansible/collections/
+## fin
+deactivate
+
+# Mise à jour du repertoire ansible madeb 11
+## Dépendance
+if ( sudo apt-cache policy jq | grep Installé | grep -q aucun ) ; then
+	sudo apt-get -y install jq
+fi
+
+#~ wget -O /tmp/madeb.tar.gz --user-agent=Firefox --no-check-certificate "https://uabox.univ-angers.fr/index.php/s/C50ShQyw6NjT9iD/download"
+
+## Version actuelle
+OLD_VERSION=$(head -1 /opt/ansible/madeb11/Version.txt 2>/dev/null)
+## Version nouvelle : la dernière
+### Dépot Github
+repoGit="cpunivangers/madeb11"
+latest="https://api.github.com/repos/$repoGit/releases/latest"
+VERSION=$(curl -s "$latest" | jq -r '.tag_name')
+
+echo "Version disponible : $VERSION"
+echo "Version actuelle   : $OLD_VERSION"
+if [ -z "$VERSION" ] ; then
+	echo -e $rouge ERREUR Version disponible
+	exit
+fi
+if dpkg --compare-versions "$VERSION" gt "$OLD_VERSION" ; then
+	echo "Nouvelle version disponible ..."
+else
+	echo "Déjà la dernière version"
+	exit
+fi
+
+sudo rm -f /tmp/madeb.tar.gz
+
+HREF=$(curl -s "$latest" | jq -r '.tarball_url')
+wget -O /tmp/madeb.tar.gz $HREF
+sudo tar -xzf /tmp/madeb.tar.gz -C /opt/ansible/ --one-top-level=madeb11 --strip-components 1
+
+sudo chown -R $USER:sudo /opt/ansible
+
+# ansible log
+sudo mkdir -m 2755 -p /var/log/ansible
+sudo chown -R $USER:sudo /var/log/ansible
+
+echo "$VERSION" > /opt/ansible/madeb11/Version.txt
+
+if [ "$OPTION" = "download" ] ; then
+	exit
+fi
+
+cd /opt/ansible/madeb11
+bash madeb.sh
